1a0a0b1c7c5c071db9a19f883265fad0773f8200
LOKESH KUMAR <lokesh.kumar@knoldus.com>
2021-10-11 18:34:01 +0530

    integration exercise

--- calculas.py
+++ calculas.py
@@ -23,3 +23,18 @@ print('second derivative w.r.t. x: ',
 # Finding second derivative of exp with respect to y
 derivative2_y = sym.diff(exp, y, 2)
 print('second derivative w.r.t. y: ', derivative2_y)
+
+# Indefinite integration of cos(x) w.r.t. dx
+integral1 = sym.integrate(sym.cos(x), x)
+print('indefinite integral of cos(x): ',
+      integral1)
+ 
+# definite integration of cos(x) w.r.t. dx between -1 to 1
+integral2 = sym.integrate(sym.cos(x), (x, -1, 1))
+print('definite integral of cos(x) between -1 to 1: ',
+      integral2)
+ 
+# definite integration of exp(-x) w.r.t. dx between 0 to ∞
+integral3 = sym.integrate(sym.exp(-x), (x, 0, sym.oo))
+print('definite integral of exp(-x) between 0 to ∞: ',
+      integral3)
